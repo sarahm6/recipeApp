36f919e3ca81d9f6c51c96f42acefef570f138b7
Sarah Memon <sarahmemon@gmail.com>
2023-03-09 21:26:23 -0500

    delete button working

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+  
 
 function Recipe({
+  _id: id ,
+  handleDelete,
   name = 'Samosas',
   content = 'Quick and spicy',
   category = 'snack',
   imageURL = 'http://img.sndimg.com/food/image/upload/w_614,h_461/v1/img/recipes/36/78/07/picgLdb1z.jpg',
   ingredients = "Lentils"
 }) {
+
+  
+
   return (
     <article className='card'>
       <div className='box'>
         <img src={imageURL} />
       </div>
       <header className='card-content'>
-        <span className='card-category chicken'>{category}</span>
+        <span className='card-category chicken margin-right-5'>{category}</span>
+        <Button className='del-btn' size="sm" variant="outline-danger" onClick={() => handleDelete(id)}>Delete</Button>
         <span className='card-header'>{name}</span>
         <span className='card-desc'>{content}</span>
       </header>
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,102 +1,36 @@
 import Recipe from './Recipe';
 // import { useState, useEffect } from 'react';
 import useData from '../hooks/useData';
+import axios from 'axios';
+// import useSWR, { useSWRConfig } from 'swr'
+
 
 function Recipes() {
-    const { data, isLoading, isError } = useData('/recipes');
-    console.log(data)
+  // const { mutate } = useSWRConfig()
+    const { data } = useData('/recipes');
+
+    const handleDelete = (id) => {
+      async function deleteRecipe() {
+        const endPoint = process.env.REACT_APP_BASE_URL+"/recipes/"+id;
+        try {
+          await axios.delete(endPoint);
+            // mutate('/recipes')
+            window.location.reload();
+  
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      deleteRecipe();
+    }
 
   return (
     <section className='wrapper product'>
       <h2 className='section-name'>our delicious collections</h2>
       {data?.map((recipe) => {
-      return <Recipe {...recipe} key ={recipe._id}/>
+      return <Recipe {...recipe} key ={recipe._id} handleDelete={handleDelete}/>
       })}
-      {/* <article className='card featured'>
-        <div className='box'>
-          <img src='http://img.sndimg.com/food/image/upload/w_614,h_461/v1/img/recipes/36/78/07/picgLdb1z.jpg' />
-        </div>
-        <header className='card-content'>
-          <span className='card-category chicken'>food category</span>
-          <span className='card-header'>recipe name</span>
-          <span className='card-desc'>recipe short description goes here.</span>
-        </header>
-        <footer className='card-content'>
-          <div className='view_recipe'>
-            <span className='view-recipe'>
-              <a href='#'>View Recipe</a>
-            </span>
-          </div>
-        </footer>
-      </article>
-      <article className='card'>
-        <div className='box'>
-          <img src='http://img.sndimg.com/food/image/upload/w_614,h_461/v1/img/recipes/36/78/07/picgLdb1z.jpg' />
-        </div>
-        <header className='card-content'>
-          <span className='card-category chicken'>food category</span>
-          <span className='card-header'>recipe name</span>
-          <span className='card-desc'>recipe short description goes here.</span>
-        </header>
-        <footer className='card-content'>
-          <div className='view_recipe'>
-            <span className='view-recipe'>
-              <a href='#'>View Recipe</a>
-            </span>
-          </div>
-        </footer>
-      </article>
-      <article className='card'>
-        <div className='box'>
-          <img src='http://img.sndimg.com/food/image/upload/w_614,h_461/v1/img/recipes/36/78/07/picgLdb1z.jpg' />
-        </div>
-        <header className='card-content'>
-          <span className='card-category chicken'>food category</span>
-          <span className='card-header'>recipe name</span>
-          <span className='card-desc'>recipe short description goes here.</span>
-        </header>
-        <footer className='card-content'>
-          <div className='view_recipe'>
-            <span className='view-recipe'>
-              <a href='#'>View Recipe</a>
-            </span>
-          </div>
-        </footer>
-      </article>
-      <article className='card'>
-        <div className='box'>
-          <img src='http://img.sndimg.com/food/image/upload/w_614,h_461/v1/img/recipes/36/78/07/picgLdb1z.jpg' />
-        </div>
-        <header className='card-content'>
-          <span className='card-category chicken'>food category</span>
-          <span className='card-header'>recipe name</span>
-          <span className='card-desc'>recipe short description goes here.</span>
-        </header>
-        <footer className='card-content'>
-          <div className='view_recipe'>
-            <span className='view-recipe'>
-              <a href='#'>View Recipe</a>
-            </span>
-          </div>
-        </footer>
-      </article>
-      <article className='card'>
-        <div className='box'>
-          <img src='http://img.sndimg.com/food/image/upload/w_614,h_461/v1/img/recipes/36/78/07/picgLdb1z.jpg' />
-        </div>
-        <header className='card-content'>
-          <span className='card-category chicken'>food category</span>
-          <span className='card-header'>recipe name</span>
-          <span className='card-desc'>recipe short description goes here.</span>
-        </header>
-        <footer className='card-content'>
-          <div className='view_recipe'>
-            <span className='view-recipe'>
-              <a href='#'>View Recipe</a>
-            </span>
-          </div>
-        </footer>
-      </article> */}
+      
     </section>
   );
 }
